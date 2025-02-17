1db7a2e8e7832f7621df384aa260405102b1083f
Mhernp04 <mhernp04@estudiantes.unileon.es>
2025-02-17 13:55:46 +0100

    Add files via upload

---
+++ Calcula_nota.c
@@ -0,0 +1,28 @@
+int main () {
+	int num = 0;
+	printf ("Esbribe la nota numerica ");
+	scanf("%d", &num);
+	if (num == 5)|| (num == 6) {
+		printf ("Aprobado ");
+		}
+	
+	else {
+	 if (num == 7)|| (num == 8)
+	 printf ("Notale ");}
+	 
+	 else {
+	 if (num == 9)|| (num == 10)
+	 printf ("sobresaliente ");}
+	 
+	 else {
+	 if (num >= 0)|| (num < 5)
+	 printf ("Notale ");}
+	 
+	 else {
+	 printf ("Error, tiene que estar entre 0 y 10 ");}
+	
+	
+	printf ("Fin del algoritmo ");
+	
+	
+}
---
+++ Detecta_tres.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+	int num = 0;
+	printf ("Esbribe un numero ");
+	scanf("%d", &num);
+	if (num == 3) {
+		printf ("Has introducido un 3 ");
+		}
+	
+	else{ printf ("No has introducido un 3 sino un: %d\n", num);}
+	
+	
+	printf ("Fin del algoritmo ");
+	
+	
+}
