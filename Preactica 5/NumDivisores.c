055be026b5c8503ccaf186fb82ebbb1230cbe153
Mhernp04 <mhernp04@estudiantes.unileon.es>
2025-02-24 13:48:54 +0100

    Add files via upload

---
+++ Preactica 5/NumDivisores.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main (){
+	int num;
+	int div;
+	int result;
+	printf ("Introduce una un numero entero positivo ");
+	scanf ("%d",&num);
+	if (num<0) {
+		printf ("Numero no valido, tiene que ser positivo y entero ");
+	}
+	int calculaDivisores (int num) {
+		for (div=1; div<=num; div++){
+			return div++;
+			
+			do {
+			 result = num/div++;
+			}
+			while (result == );
+			
+			if (result != int){
+				return 0;
+			}
+			
+			
+		}  
+	}	
+	
+	printf("el numero de divisores son = %d ", result);
+	return 0;
+}
