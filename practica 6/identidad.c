3f63045a4ceb38a811f3c9aabd7983d4d6e612a0
Mhernp04 <mhernp04@estudiantes.unileon.es>
2025-02-26 12:12:28 +0100

    Add files via upload

---
+++ practica 6/identidad.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <time.h>
+
+
+
+int main (){
+	int identidad[] = new int [9][9];
+	
+	int identidad[9][9]={{1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {valor1fila2, valor2fila2, valor3fila2,
+	valornfila2},{valor1filam, valor2filam, valor3filam, valornfilam}};	
+	
+}
---
+++ practica 6/sumaElementos.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main (){
+	
+	int num_aleatorio=rand() % (10+1);
+	
+	int identidad[10] = {1, 2 , 3, 4 ,5 ,6 ,7 ,8 ,9 ,10};
+	
+	
+		int i =0;
+		
+		for (i=0; i<=10; i++) {
+		 
+		return i;
+		printf (" %d", &i);
+		}
+		
+
+
+}
